10555f30152176e1cf814a5f2a3bddb0f4cb8797
Maksim Fesenko <DrResist@gmail.com>
2021-09-17 09:57:12 +0300

    add black formatter, format code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import nltk
+
 # nltk.download()
 
 """
@@ -20,14 +21,14 @@ def read_file(filename) -> dict:
     xml_tree = etree.parse(filename).getroot()
     headers = [news[0].text for news in xml_tree[0]]
     text = [news[1].text for news in xml_tree[0]]
-
+    
     return dict(zip(headers, text))
 
 
 # Tokenize the texts.
 def tokenize_text(dictionary: dict) -> dict:
     tokens = []
-    for header, text in dictionary.items():
+    for _, text in dictionary.items():
         tokens.append(nltk.tokenize.word_tokenize(str(text).lower()))
     return dict(zip(dictionary.keys(), tokens))
 
@@ -35,30 +36,27 @@ def tokenize_text(dictionary: dict) -> dict:
 # For each story, find the most frequent tokens that appear in it.
 def count_frequency(dictionary: dict) -> dict:
     freq_dict = []
-    for header, tokens in dictionary.items():
+    for _, tokens in dictionary.items():
         tokens = sorted(tokens, reverse=True)
         fdist = nltk.FreqDist(tokens)
-        freq_dict.append(
-            [i[0] for i in fdist.most_common(5)]
-        )
+        freq_dict.append([i[0] for i in fdist.most_common(5)])
     return dict(zip(dictionary.keys(), freq_dict))
 
 
 # Print the headline of each news story the five most frequent tokens in descending order.
 def print_output(dictionary: dict):
     for header, most_frequent_word in dictionary.items():
-        print(header + ':')
-        print(' '.join(most_frequent_word))
+        print(header + ":")
+        print(" ".join(most_frequent_word))
 
-    return 0
 
 
 def main():
-    header_text = read_file(filename='news.xml')  # {[headers] : [text]}
+    header_text = read_file(filename="news.xml")  # {[headers] : [text]}
     header_token = tokenize_text(header_text)  # {[headers]: [tokens]}
     header_most_common = count_frequency(header_token)  # {[headers]: [most common]}
     print_output(header_most_common)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
